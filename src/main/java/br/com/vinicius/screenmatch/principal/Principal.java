a3da7dbf6acc74a047b10dafd1cd0957074e06c3
ViniciusPolo <admviniciuspolo@gmail.com>
2024-03-21 22:05:06 -0300

    Antes melhorar for's

--- src/main/java/br/com/vinicius/screenmatch/principal/Principal.java
+++ src/main/java/br/com/vinicius/screenmatch/principal/Principal.java
@@ -1,5 +1,6 @@
 package br.com.vinicius.screenmatch.principal;
 
+import br.com.vinicius.screenmatch.model.DadosEpisodio;
 import br.com.vinicius.screenmatch.model.DadosSerie;
 import br.com.vinicius.screenmatch.model.DadosTemporada;
 import br.com.vinicius.screenmatch.service.ConsumoApi;
@@ -26,12 +27,19 @@ public class Principal {
 
         List<DadosTemporada> temporadas = new ArrayList<>();
 
-		for (int i = 0; i <= dados.totalTemporadas(); i++){
+		for (int i = 1; i <= dados.totalTemporadas(); i++){
 			json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") +"&Season=" + i + API_KEY);
 			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
 			temporadas.add(dadosTemporada);
 		}
 		temporadas.forEach(System.out::println);
+
+        for (int i = 0; i < dados.totalTemporadas(); i++){
+            List <DadosEpisodio> episodiosTemporada = temporadas.get(i).episodios();
+            for(int j = 0; j < episodiosTemporada.size(); j++){
+                System.out.println(episodiosTemporada.get(j).titulo());
+            }
+        }
 
     }
 }
