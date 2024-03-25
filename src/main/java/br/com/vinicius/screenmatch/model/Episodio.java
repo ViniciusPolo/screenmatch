4a1f6abc9ce33607db3b09be5f33700935e6c1ac
ViniciusPolo <admviniciuspolo@gmail.com>
2024-03-25 10:21:04 -0300

    JPA Chave Estrangeira, Cascade, Fetch Lazy and Eager

--- src/main/java/br/com/vinicius/screenmatch/model/Episodio.java
+++ src/main/java/br/com/vinicius/screenmatch/model/Episodio.java
@@ -1,18 +1,30 @@
 package br.com.vinicius.screenmatch.model;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import java.time.DateTimeException;
 import java.time.LocalDate;
 @Data
 @ToString
+@Entity
+@Table(name = "episodios")
+@NoArgsConstructor
 public class Episodio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
     private Integer temporada;
     private String titulo;
     private Integer numeroEpisodio;
     private Double avaliacao;
     private LocalDate dataLancamento;
+
+    @ManyToOne
+    private Serie serie;
+
 
     public Episodio(Integer numeroTemporada, DadosEpisodio dadosEpisodio) {
         this.temporada = numeroTemporada;
--- src/main/java/br/com/vinicius/screenmatch/model/Serie.java
+++ src/main/java/br/com/vinicius/screenmatch/model/Serie.java
@@ -30,7 +30,7 @@ public class Serie {
     private String poster;
     private String sinopse;
 
-    @Transient
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private List<Episodio> episodios = new ArrayList<>();
 
     public Serie(DadosSerie dadosSerie) {
@@ -42,6 +42,11 @@ public class Serie {
         this.poster = dadosSerie.poster();
         this.sinopse = dadosSerie.sinopse();
         //this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse());
+    }
+
+    public void setEpisodios(List<Episodio> episodios) {
+        episodios.forEach(e -> e.setSerie(this));
+        this.episodios = episodios;
     }
 }
 
