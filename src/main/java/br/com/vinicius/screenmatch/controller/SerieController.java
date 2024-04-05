ba8a62122ae913631810458df1cfd4ada87c0441
ViniciusPolo <admviniciuspolo@gmail.com>
2024-04-05 13:57:11 -0300

    service com repetição

--- src/main/java/br/com/vinicius/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/vinicius/screenmatch/controller/SerieController.java
@@ -1,20 +1,18 @@
 package br.com.vinicius.screenmatch.controller;
 
 import br.com.vinicius.screenmatch.dto.SerieDTO;
-import br.com.vinicius.screenmatch.repository.SerieRepository;
-import br.com.vinicius.screenmatch.model.Serie;
+import br.com.vinicius.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
     @Autowired
-    private SerieRepository repositorio;
+    private SerieService servico;
 
     @GetMapping("/")
     public String inicio(){
@@ -24,9 +22,11 @@ public class SerieController {
 
     @GetMapping("/series")
     public List<SerieDTO> obterSeries(){
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return servico.obterTodasAsSeries();
+    }
+
+    @GetMapping("/series/top5")
+    public List<SerieDTO> obterSeriesTop5(){
+        return servico.obterTop5Series();
     }
 }
--- src/main/java/br/com/vinicius/screenmatch/service/SerieService.java
+++ src/main/java/br/com/vinicius/screenmatch/service/SerieService.java
@@ -1,2 +1,29 @@
-package br.com.vinicius.screenmatch.service;public class SerieService {
+package br.com.vinicius.screenmatch.service;
+
+import br.com.vinicius.screenmatch.dto.SerieDTO;
+import br.com.vinicius.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class SerieService {
+    @Autowired
+    private SerieRepository repositorio;
+
+    public List<SerieDTO> obterTodasAsSeries(){
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
+
+    public List<SerieDTO> obterTop5Series(){
+        return repositorio.findTop5ByOrderByAvaliacaoDesc()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
+    }
 }
