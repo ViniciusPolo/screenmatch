3ea747976f7d141087c917328bada255ba742bbd
ViniciusPolo <admviniciuspolo@gmail.com>
2024-04-05 19:55:15 -0300

    antes correção top 5

--- src/main/java/br/com/vinicius/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/vinicius/screenmatch/controller/SerieController.java
@@ -4,29 +4,36 @@ import br.com.vinicius.screenmatch.dto.SerieDTO;
 import br.com.vinicius.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/series")
 public class SerieController {
 
     @Autowired
     private SerieService servico;
 
-    @GetMapping("/")
-    public String inicio(){
-        return"<h1>Bem Vindo ao ScreenMatch</h1>" +
-                "<p>Essa rota é somente para teste de LiveReload! </p>";
-    }
-
-    @GetMapping("/series")
+    @GetMapping
     public List<SerieDTO> obterSeries(){
         return servico.obterTodasAsSeries();
     }
 
-    @GetMapping("/series/top5")
+    @GetMapping("/top5")
     public List<SerieDTO> obterSeriesTop5(){
         return servico.obterTop5Series();
+    }
+
+    @GetMapping("/lancamentos")
+    public List<SerieDTO> obterLancamentos(){
+        return servico.obterLancamentos();
+    }
+
+    @GetMapping("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id){
+        return servico.obterPorId(id);
     }
 }
--- src/main/java/br/com/vinicius/screenmatch/service/SerieService.java
+++ src/main/java/br/com/vinicius/screenmatch/service/SerieService.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
+
 public class SerieService {
     @Autowired
     private SerieRepository repositorio;
@@ -24,9 +26,22 @@ public class SerieService {
 
     }
 
+    public List<SerieDTO> obterLancamentos() {
+        return converteDados(repositorio.findTop5ByOrderByEpisodiosDataLancamentoDesc());
+    }
+
     private List<SerieDTO> converteDados(List<Serie> series){
         return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if (serie.isPresent()){
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
