fc84c40fcbaa2e76860efdde307296454a7c3efb
ViniciusPolo <admviniciuspolo@gmail.com>
2024-04-05 14:01:18 -0300

    service sem repetição

--- src/main/java/br/com/vinicius/screenmatch/service/SerieService.java
+++ src/main/java/br/com/vinicius/screenmatch/service/SerieService.java
@@ -1,6 +1,7 @@
 package br.com.vinicius.screenmatch.service;
 
 import br.com.vinicius.screenmatch.dto.SerieDTO;
+import br.com.vinicius.screenmatch.model.Serie;
 import br.com.vinicius.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,15 +15,17 @@ public class SerieService {
     private SerieRepository repositorio;
 
     public List<SerieDTO> obterTodasAsSeries(){
-        return repositorio.findAll()
-                .stream()
-                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
-                .collect(Collectors.toList());
+        return converteDados(repositorio.findAll());
+
     }
 
     public List<SerieDTO> obterTop5Series(){
-        return repositorio.findTop5ByOrderByAvaliacaoDesc()
-                .stream()
+        return converteDados(repositorio.findTop5ByOrderByAvaliacaoDesc());
+
+    }
+
+    private List<SerieDTO> converteDados(List<Serie> series){
+        return series.stream()
                 .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
     }
